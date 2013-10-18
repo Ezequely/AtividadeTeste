ea2bfb2e7c36609f92c33f02bdf681d6d0ac56ff
Ezequely Silva de Freitas <ezequelyfreitas@gmail.com>
2013-10-18 14:08:32 -0300

    Correção do método

--- src/main/Expedicao.java
+++ src/main/Expedicao.java
@@ -7,7 +7,11 @@ public class Expedicao {
 	
 	public boolean sedex(Bola bola, Caixa caixa){
 		boolean saida = false;
-		if(bola.diametro <= caixa.altura && bola.diametro <= caixa.largura && bola.diametro <= caixa.profundidade)
+		if(bola.diametro >=1 && bola.diametro<=10000)
+			if(caixa.altura >=1 && caixa.altura<=10000)
+				if(caixa.largura >=1 && caixa.largura<=10000)
+					if(caixa.profundidade >=1 && caixa.profundidade<=10000)
+						if(bola.diametro <= caixa.altura && bola.diametro <= caixa.largura && bola.diametro <= caixa.profundidade)
 			saida = true;
 		return saida;
 	}
